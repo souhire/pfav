c84464aaa63bcba8af67f9b270ebed76b6b7ad49
Hai NGUYEN VAN <hai.ngv@etu.univ-paris-diderot.fr>
2013-03-14 22:28:14 +0100

    ajout de fonctions de récupération d'un emplacement géo (lat, long) à partir d'un emplacement formel et récupération d'un restaurant proche de cet emplacement

--- test.ml
+++ test.ml
@@ -2,6 +2,8 @@
 #require "curl";;
 #require "yojson";;
 
+type location = float * float;;
+
 (* Fonction string_of_uri *)
 (* Copyright RDC *)
 let string_of_uri ?(headers=[]) ?query uri = 
@@ -126,10 +128,95 @@ let rec genre_of_allocine_json (j : Yojson.Safe.json) : string list =
     | _ -> failwith "Genre field not found"
 ;;
 
+(**
+   L'utilisateur donne sa position géographique à l'aide d'un emplacement informel (nom de rue, métro)
+*)
+(*
+  Une instance possible d'AST JSON par Google Places pour la position géographique d'un emplacement informel
+  https://maps.googleapis.com/maps/api/place/textsearch/json?sensor=true&query=rue%20camille%20desmoulins%20cachan&key=AIzaSyA2kG_PpWlQG91CVNXKfyeaKdxbuUK-CeE
+*)
+let google_place_of_informal_location_example = "{\"html_attributions\" : [],\"results\" : [{\"formatted_address\" : \"Rue Camille Desmoulins, 94230 Cachan, France\",\"geometry\" : {\"location\" : {\"lat\" : 48.79287020,\"lng\" : 2.33313570}},\"icon\" : \"http://maps.gstatic.com/mapfiles/place_api/icons/geocode-71.png\",\"id\" : \"2cc24e810b79496d378a42193017752ee9386dd7\",\"name\" : \"Rue Camille Desmoulins\",\"reference\" : \"CqQBlAAAAGLxwhv6v7vfsVUC-84upQyOxLHj5CrcvIxZ4fCjVXX0yfvd2vrCx2wdFpLDa-pVMBSNR3DILkQklONvk94cBKB_1_4vYr5X9naQJhi7SNJrHgCJjsxRgK6PoM-10-jpjU_XPGrQn7sSXTxbd8Yu2IkWMUvIcUZlEX4MUsuiQVF8KHsc4S_27qndMDOxg1_dyLg3p3DiIr5BdAN5A1CpP0ASEBq770oZbf0Ii3QqfxtRpa0aFOihM0ApZOZw9nCBw-WBQyTs39uH\",\"types\" : [ \"route\" ]}],\"status\" : \"OK\"}";;
 
+(* Position géographique (latitude, longitude) à partir d'un emplacement informel *)
+let geographic_location_of_informal_location (l : string) : float * float =
+  (* a changer en attendant le support de HTTPS *)
+  let json_ast = Yojson.Safe.from_string google_place_of_informal_location_example in
+  match json_ast with
+    | `Assoc (l) -> (
+      match (List.assoc "results" l) with
+	| `List ((`Assoc l') :: _) -> (
+	  match (List.assoc "geometry" l') with
+	    | `Assoc l'' -> (
+	      match (List.assoc "location" l'') with 
+		| `Assoc (("lat", `Float f1) :: ("lng", `Float f2) :: _) -> (f1, f2)
+		| _ -> failwith "AST problem"
+	    )
+	    | _ -> failwith "AST problem"
+	)
+	| _ -> failwith "AST problem"
+    )
+    | _ -> failwith "AST problem"
+;;
 
+(**
+   Googles Places renvoie tous les restaurants à un emplacement géographique suivant un rayon précis
+*)
+(*
+  Une instance possible d'AST JSON par Google Places pour la position géographique d'un restaurant proche d'un emplacement géographique
+  https://maps.googleapis.com/maps/api/place/nearbysearch/json?language=fr&location=48.7928702,2.3331357&radius=100&sensor=true&key=AIzaSyA2kG_PpWlQG91CVNXKfyeaKdxbuUK-CeE&types=restaurant
+*)
+let restaurants_example = "{\"html_attributions\" : [],\"results\" : [{\"geometry\" : {\"location\" : {\"lat\" : 48.7936230,\"lng\" : 2.3334760}},\"icon\" : \"http://maps.gstatic.com/mapfiles/place_api/icons/restaurant-71.png\",\"id\" : \"8cad79432858e1a9629c6f370b4d1da5b7a1c392\",\"name\" : \"La Maison des Pizzas\",\"reference\" : \"CoQBcgAAAHP9UbDj2XB5MPvHOHrtv3IueOAVfL-va8apcyDF17qrM48KbmU_ebDzentFZf_EsmUYiSI4OiOLgT0USGQ4rNkptI3wd3p0w1_bBkv69mgYJVbEOZ3TiJvlARBs7Tt9L0uwPrTlfrWMfkAyDPlM6aCYJNdKmM8wax3dWbwxlbIfEhBahtZkX1s6l2iIprQmAXwbGhSRdna5Krw6dDchgkN6wECVevKjcg\",\"types\" : [ \"restaurant\", \"food\", \"establishment\" ],\"vicinity\" : \"26 Rue Camille Desmoulins, Cachan\"}],\"status\" : \"OK\"}";;
+
+let restaurants_at_geographic_location (emplacement : location) (radius : int) : float * float * string * string =
+  (* a changer en attendant le support de HTTPS *)
+  (* regler le pb de choisir l'emplacement le plus et NON le plus "interessant" *)
+  let json_ast = Yojson.Safe.from_string restaurants_example in
+  let (lat_location, lng_location) = match json_ast with
+    | `Assoc (l) -> (
+      match (List.assoc "results" l) with
+	| `List ((`Assoc l') :: _) -> (
+	  match (List.assoc "geometry" l') with
+	    | `Assoc l'' -> (
+	      match (List.assoc "location" l'') with 
+		| `Assoc (("lat", `Float f1) :: ("lng", `Float f2) :: _) -> (f1, f2)
+		| _ -> failwith "AST problem"
+	    )
+	    | _ -> failwith "AST problem"
+	)
+	| _ -> failwith "AST problem"
+    )
+    | _ -> failwith "AST problem"
+  and name = match json_ast with
+    | `Assoc (l) -> (
+      match (List.assoc "results" l) with
+	| `List ((`Assoc l') :: _) -> (
+	  match (List.assoc "name" l') with
+	    | `String s -> s
+	    | _ -> failwith "AST problem"
+	)
+	| _ -> failwith "AST problem"
+    )
+    | _ -> failwith "AST problem"
+  and address = match json_ast with
+    | `Assoc (l) -> (
+      match (List.assoc "results" l) with
+	| `List ((`Assoc l') :: _) -> (
+	  match (List.assoc "vicinity" l') with
+	    | `String s -> s
+	    | _ -> failwith "AST problem"
+	)
+	| _ -> failwith "AST problem"
+    )
+    | _ -> failwith "AST problem"
+  in (lat_location, lng_location, name, address)
+;;
+
+(**
+   Tests et debug
+*)
+geographic_location_of_informal_location "";;
 allocine_id_of_movie_name movie_name;;
-
+restaurants_at_geographic_location (0., 0.) 0;;
 
 
 
